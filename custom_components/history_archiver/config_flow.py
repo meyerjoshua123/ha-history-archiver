c49b4a0d617d723c53b3a08cac7fe3da76cb9a3c
meyerjoshua123 <83791947+meyerjoshua123@users.noreply.github.com>
2026-02-20 20:47:23 +1100

    Update config_flow.py

---
+++ custom_components/history_archiver/config_flow.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import voluptuous as vol
+from homeassistant import config_entries
+from .const import DOMAIN
+
+class HistoryArchiverConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle the config flow."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(title="History Archiver", data={})
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({})
+        )
